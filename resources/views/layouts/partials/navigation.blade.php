e42b18ee8f42651ce35b26a99052236662451878
chen shu ting <a.0066715@gmail.com>
2021-10-28 22:38:48 +0800

    建立共用區塊

---
+++ resources/views/layouts/partials/navigation.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<ul>
+    <li>
+        <a href="#">Item1</a>
+    </li>
+    <li>
+        <a href="#">Item2</a>
+    </li>
+</ul>
+
+</body>
+</html>
+<?php

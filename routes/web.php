9597eeb1fb9c40422bea79d8a55bf480c591e733
chen shu ting <a.0066715@gmail.com>
2021-12-07 21:57:51 +0800

    回傳home1頁面

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,10 @@ Route::get('/', function () {
 
 //Route::get('home',function(){
     //return view('home.index');});
-
+	
+Route::get('home1',function(){
+return view('home1');
+});
 
 Route::get('home',[HomeController::class,'index'])->name('home.index');
 
